0b72e8680e59ba4afaf23235765cef3fb223c8a8
HarshGopwar888 <harshgopwar1805@gmail.com>
2025-05-11 21:02:23 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS codde as new version
+
+// alert (3 +3) // we are using Nodejs, not browser
+
+console.log (3 + 3) 
+console.log("Harsh")
+
+let name= "Harsh"
+let age= 22
+let isLive= false
+
+/*
+dataTypes 8 
+
+primitive 
+         number => 2 to power 53
+         bigint
+         string => ""
+         boolean => true/false
+         null => standalone value
+         undefined => 
+         symbol => unique            
+*/
+
+//object
+console.log(typeof  "Harsh"); //String
+console.log(typeof  age); // number
+console.log(typeof  null); // object
+console.log(typeof  undefined); //undefined  
