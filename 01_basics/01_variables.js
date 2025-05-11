9cb831e1411e01ad27323bb4454fa452bdfb244d
HarshGopwar888 <harshgopwar1805@gmail.com>
2025-05-11 21:01:40 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId= 14453 //Locked can not be changed 
+let accountEmail= "harsh@google.com"
+var accountPassword="12345"
+accountCity= "Pune"
+let accountState;
+
+//accountId = 2 
+accountEmail= "Harsh@.com"
+accountPassword="1221"
+accountCity ="Mumbai"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) // Help to print in Tables
+
+/*
+Prefer not to use var
+Because of issue in Block scope & Function scope
+*/
