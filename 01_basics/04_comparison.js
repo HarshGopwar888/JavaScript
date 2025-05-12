dc9436249ec887896b90ec02a14d06b0ac037b4d
Harsh Gopwar <harshgopwar1805@gmail.com>
2025-05-12 17:01:10 +0000

    Comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+console.log(2 > 1) // true
+console.log("3" > 1)  // true
+
+console.log(null > 0); //false
+console.log(null == 0 ); //false
+console.log(null >= 0 ); //true
+
+console.log(undefined ==0); // false
+
+console.log("2" == 2); //true
+
+
+
